4e96486c568a1fa9aaad6ca171c1e706bf6dad25
z2labplus <858570636@qq.com>
2023-09-20 00:33:23 +0800

    build

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,11 @@ import tailwind from '@astrojs/tailwind';
 import netlify from "@astrojs/netlify/functions";
 import preact from "@astrojs/preact";
 
+import cloudflare from "@astrojs/cloudflare";
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), preact()],
   output: "server",
-  adapter: netlify()
+  adapter: cloudflare()
 });
