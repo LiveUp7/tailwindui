93eeca64f35fcda0810272f0b886e6fa3b3b99b0
z2labplus <858570636@qq.com>
2023-09-20 00:55:19 +0800

    modify

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -2,9 +2,9 @@ import rss, { pagesGlobToRssItems } from '@astrojs/rss';
 
 export async function GET() {
   return rss({
-    title: 'Astro Learner | Blog',
-    description: 'My journey learning Astro',
-    site: 'https://my-blog-site.netlify.app',
+    title: 'Astro Learner | Blog 45',
+    description: 'My journey learning Astro 23',
+    site: 'https://tailwindui-6re.pages.dev',
     items: await pagesGlobToRssItems(import.meta.glob('./**/*.md')),
     customData: `<language>en-us</language>`,
   });
